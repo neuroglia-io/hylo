4bea9701d3d823ad49b2fb55564bb73f1603bb96
Charles d'Avernas <charles.davernas@neuroglia.io>
2023-05-03 13:25:52 +0200

    Unit test fix attempt #3

--- tests/unit-tests/Cases/DatabaseTestsBase.cs
+++ tests/unit-tests/Cases/DatabaseTestsBase.cs
@@ -215,7 +215,7 @@ public abstract class DatabaseTestsBase
         {
             await resourceRepository.AddAsync(FakeResourceWithSpecAndStatus.Create(@namespace)).ConfigureAwait(false);
         }
-        await Task.Delay(150);
+        await Task.Delay(100);
 
         //assert
         processedWatchEvents.ToList().Should().HaveCount(resourceCount);
