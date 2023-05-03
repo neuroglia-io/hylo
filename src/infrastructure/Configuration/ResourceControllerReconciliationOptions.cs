940c8e950d34e4c194de72d7d07787f11cde5fcc
Charles d'Avernas <charles.davernas@neuroglia.io>
2023-05-03 13:18:59 +0200

    feat(Infrastructure): Added the IResourceController service and its default implementation

---
+++ src/infrastructure/Configuration/ResourceControllerReconciliationOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace Hylo.Infrastructure.Configuration;
+
+/// <summary>
+/// Represents the options used to configure the reconciliation loop of a <see cref="ResourceController{TResource}"/>
+/// </summary>
+public class ResourceControllerReconciliationOptions
+{
+
+    /// <summary>
+    /// Gets/sets the interval at which to perform resource reconciliation
+    /// </summary>
+    public virtual TimeSpan Interval { get; set; } = TimeSpan.FromSeconds(30);
+
+}
---
+++ src/infrastructure/Services/Interfaces/IResourceController.cs
@@ -0,0 +1,17 @@
+﻿namespace Hylo.Infrastructure.Services;
+
+/// <summary>
+/// Defines the fundamentals of a service used to control <see cref="IResource"/>s of the specified type
+/// </summary>
+/// <typeparam name="TResource">The type of <see cref="IResource"/>s to control</typeparam>
+public interface IResourceController<TResource>
+    : IHostedService, IObservable<IResourceWatchEvent<TResource>>, IDisposable, IAsyncDisposable
+    where TResource : class, IResource, new()
+{
+
+    /// <summary>
+    /// Gets a <see cref="List{T}"/> containing the current state of controlled <see cref="IResource"/>s
+    /// </summary>
+    List<TResource> Resources { get; }
+
+}
